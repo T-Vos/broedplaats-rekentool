2d9e8f3199e10c99003e5d27d454cd7fdd2039c1
Thomas Vos <thomasvos@outlook.com>
2024-12-20 19:57:15 +0100

    removes count up

--- ui/component-tree.tsx
+++ ui/component-tree.tsx
@@ -50,25 +50,6 @@ const List = ({ items, depth }: { items: Item[]; depth: number }) => {
                 {item.name}
                 <span className="text-white/40">{'>'}</span>
               </div>
-
-              <div
-                className={clsx(
-                  'rounded-md bg-gray-800 px-2 py-0.5 text-xs tracking-wide text-white/50',
-                  {
-                    'animate-[fadeToTransparent_1s_ease-in-out_forwards_1]':
-                      item.type === 'server',
-                  },
-                )}
-              >
-                <span className="tabular-nums">
-                  {item.type === 'client' ? (
-                    item.size / 1000
-                  ) : (
-                    // <CountUp start={item.size / 1000} end={0} />
-                  )}
-                </span>{' '}
-                KB
-              </div>
             </div>
 
             {item.children ? (
@@ -113,13 +94,6 @@ export const ComponentTree = ({ items }: { items: Item[] }) => {
           <div className="space-y-6">
             <div className="space-y-3 rounded-lg bg-gray-900 p-4">
               <div className="flex items-center justify-between gap-x-3">
-                <div className="rounded-md bg-vercel-blue px-2 py-0.5 text-xs tabular-nums tracking-wider text-blue-50">
-                  <CountUp
-                    start={(clientTotal + serverTotal) / 1000}
-                    end={clientTotal / 1000}
-                  />{' '}
-                  KB
-                </div>
                 <div className="text-sm text-gray-300">Bundle Size</div>
               </div>
 
