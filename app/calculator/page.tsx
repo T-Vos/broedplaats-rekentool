ee79b3ee87eddde723d70cbf8f1d7982af876c78
Thomas Vos <thomasvos@outlook.com>
2024-12-21 16:03:30 +0100

    first step in expectation

--- app/calculator/page.tsx
+++ app/calculator/page.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+import { max, min } from 'd3';
+import { register } from 'module';
 import { useState } from 'react';
 
 interface RowData {
@@ -10,28 +12,122 @@ interface RowData {
   percArtStudios: number;
 }
 
-interface Assumptions {
-  expectedIncomeCatering: number;
-  expectedIncomeArtstudio: number;
-}
-
-const assumptions: Assumptions = {
-  expectedIncomeCatering: 100, // Example value
-  expectedIncomeArtstudio: 200, // Example value
-};
-
 export default function Page() {
-  const [income, setIncome] = useState(0);
-  const [expenses, setExpenses] = useState(0);
-  const [rows, setRows] = useState<RowData[]>([]);
-
-  const handleIncomeChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setIncome(Number(e.target.value));
-  };
-
-  const handleExpensesChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setExpenses(Number(e.target.value));
-  };
+  const [rows, setRows] = useState<RowData[]>([
+    {
+      totalSize: 500,
+      minCatering: 200,
+      percCatering: 0.3,
+      percHalls: 0.1,
+      percArtStudios: 0.6,
+    },
+    {
+      totalSize: 750,
+      minCatering: 200,
+      percCatering: 0.3,
+      percHalls: 0.1,
+      percArtStudios: 0.6,
+    },
+    {
+      totalSize: 1000,
+      minCatering: 200,
+      percCatering: 0.3,
+      percHalls: 0.1,
+      percArtStudios: 0.6,
+    },
+    {
+      totalSize: 1500,
+      minCatering: 200,
+      percCatering: 0.3,
+      percHalls: 0.1,
+      percArtStudios: 0.6,
+    },
+  ]);
+  const [variables, setVariables] = useState<any>({
+    income: {
+      artStudios: {
+        minIncomePerM2: 14,
+        maxIncomePerM2: 16,
+      },
+      catering: {
+        peoplePerM2: {
+          max: 4,
+          normal: 2,
+          terrace: 1.5,
+        },
+        expendure: {
+          perPerson: 25,
+          margin: 0.8,
+        },
+      },
+    },
+    expenses: {
+      labourCosts: {
+        minCostPerHour: 15,
+        maxCostPerHour: 20,
+        workingPersonPerHour: 2,
+        hoursUsage: 10,
+      },
+      renovation: {
+        maxCostPerM2: 500,
+        minCostPerM2: 300,
+        furnitureCater: 25000,
+      },
+      loanCosts: {
+        interestRate: 0.1,
+        duration: 10,
+        loanAmount: 500000,
+      },
+      legalCosts: {
+        notary: {
+          minCost: 500,
+          maxCost: 1000,
+        },
+        accountancy: {
+          minCost: 800,
+          maxCost: 800,
+        },
+        uboRegistration: 25,
+        registerChamberOfCommerce: 80,
+        taxationNumber: 105.5,
+        registrationStockHolders: 25,
+        exploitationPermit_withoutTerrarsse: 2200,
+        exploitationPermit_withTerrarsse: 2600,
+      },
+    },
+    energyUsage: {
+      electricity: {
+        costs: {
+          minPerkWh: 0.0667,
+          maxPerkWh: 0.6588,
+          average: 0.15735,
+        },
+        catering: {
+          minPerM2Kwh: 75,
+          maxPerM2Kwh: 250,
+        },
+        Office: {
+          minPerM2Kwh: 60,
+          maxPerM2Kwh: 100,
+        },
+      },
+      gas: {
+        costs: {
+          minPerM3: 0.2616,
+          maxPerM3: 2.783,
+          average: 0.6329516666666668,
+        },
+        catering: {
+          minPerM2Kwh: 21,
+          maxPerM2Kwh: 40,
+        },
+        Office: {
+          minPerM2m3: 12,
+          maxPerM2m3: 23,
+        },
+      },
+    },
+  });
 
   const handleRowChange = (
     index: number,
@@ -47,24 +143,40 @@ export default function Page() {
     setRows([
       ...rows,
       {
-        totalSize: 0,
-        minCatering: 0,
-        percCatering: 0,
-        percHalls: 0,
-        percArtStudios: 0,
+        totalSize: 500,
+        minCatering: 200,
+        percCatering: 0.3,
+        percHalls: 0.1,
+        percArtStudios: 0.6,
       },
     ]);
   };
 
-  const calculateExpectedIncome = (row: RowData) => {
-    const cateringIncome =
-      row.percCatering * assumptions.expectedIncomeCatering;
-    const artStudioIncome =
-      row.percArtStudios * assumptions.expectedIncomeArtstudio;
-    return cateringIncome + artStudioIncome;
+  const calculateExpactation = (row: RowData) => {
+    return {
+      income: calculateExpectedIncome(row),
+      expenses: calculateExpectedIncome(row),
+      profit: calculateExpectedIncome(row),
+    };
   };
 
-  const profit = income - expenses;
+  const calculateExpectedIncome = (row: RowData) => {
+    const cateringSize: number = Math.max(
+      row.minCatering,
+      row.totalSize * row.percCatering,
+    );
+    const cateringIncome: number =
+      cateringSize *
+      variables.income.catering.peoplePerM2.normal *
+      variables.income.catering.expendure.perPerson *
+      variables.income.catering.expendure.margin;
+    const artSize: number =
+      (row.totalSize - cateringSize - row.percHalls * row.totalSize) *
+      row.percArtStudios;
+    const artStudioIncome: number =
+      artSize * variables.income.artStudios.minIncomePerM2;
+    return cateringIncome + artStudioIncome;
+  };
 
   return (
     <>
@@ -177,24 +289,34 @@ export default function Page() {
           <div className="space-y-9">
             <div className="prose prose-sm prose-invert max-w-none">
               <h1 className="text-xl font-bold">Model results</h1>
-              <p>Profit: {profit}</p>
               <h2 className="mt-6 text-lg font-bold">Expected Income Table</h2>
               <table className="min-w-full bg-gray-800 text-white">
                 <thead>
                   <tr>
                     <th className="px-4 py-2">Total Size</th>
                     <th className="px-4 py-2">Expected Income</th>
+                    <th className="px-4 py-2">Expected Expense</th>
+                    <th className="px-4 py-2">Expected Profit</th>
                   </tr>
                 </thead>
                 <tbody>
-                  {rows.map((row, index) => (
-                    <tr key={index}>
-                      <td className="border px-4 py-2">{row.totalSize}</td>
-                      <td className="border px-4 py-2">
-                        {calculateExpectedIncome(row)}
-                      </td>
-                    </tr>
-                  ))}
+                  {rows.map((row, index) => {
+                    const expectation = calculateExpactation(row);
+                    return (
+                      <tr key={index}>
+                        <td className="border px-4 py-2">{row.totalSize}</td>
+                        <td className="border px-4 py-2">
+                          {expectation.income}
+                        </td>
+                        <td className="border px-4 py-2">
+                          {expectation.expenses}
+                        </td>
+                        <td className="border px-4 py-2">
+                          {expectation.profit}
+                        </td>
+                      </tr>
+                    );
+                  })}
                 </tbody>
               </table>
             </div>
--- app/gas/page.tsx
+++ app/gas/page.tsx
@@ -1,21 +1,79 @@
+import { PlusIcon } from '@heroicons/react/24/outline';
+import electricity_data from './data.json';
+import { mean, median, max, min, variance, log } from 'mathjs';
+import HistogramWithDistribution from '#/ui/HistogramWithDistribution';
+import SimpleBlock from '#/ui/simple-block';
+import { subtle } from 'crypto';
 export default function Page() {
   return (
-    <div className="space-y-9">
-      <div className="prose prose-sm prose-invert max-w-none">
-        <h1 className="text-xl font-bold">Client Component Hooks</h1>
+    <>
+      <div className="rounded-lg bg-vc-border-gradient p-px shadow-lg shadow-black/20">
+        <div className="rounded-lg bg-black p-3.5 lg:p-6">
+          <div className="space-y-9">
+            <div className="prose prose-sm prose-invert max-w-none">
+              <h1 className="text-xl font-bold">Data points</h1>
 
-        <ul>
-          <li>
-            Next.js provides a number of hooks for accessing routing information
-            from client components.
-          </li>
-          <li>
-            Try navigating each page and observing the output of each hook
-            called from the current routes <code>layout.js</code> and{' '}
-            <code>page.js</code> files.
-          </li>
-        </ul>
+              <div className="grid grid-cols-1 gap-5 lg:grid-cols-2">
+                <SimpleBlock
+                  mainText={<PlusIcon className="h-5 w-5" />}
+                  subtleText="Add data point"
+                  type="green"
+                />
+                {electricity_data.map((item, index) => {
+                  return (
+                    <SimpleBlock
+                      key={index}
+                      mainText={`${item.m3} m3`}
+                      subtleText={`${item.Jaar} ${item.Maand}`}
+                    />
+                  );
+                })}
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
+      <div className="flex flex-col justify-start gap-5">
+        <div className="rounded-lg bg-vc-border-gradient p-px shadow-lg shadow-black/20">
+          <div className="rounded-lg bg-black p-3.5 lg:p-6">
+            <div className="space-y-9">
+              <div className="prose prose-sm prose-invert max-w-none">
+                <h1 className="text-xl font-bold">Descriptive statistics</h1>
+                <div className="mb-9 grid grid-cols-1 gap-5  lg:grid-cols-2">
+                  {[
+                    { func: mean, label: 'Mean price m3' },
+                    { func: median, label: 'Median price m3' },
+                    { func: max, label: 'Maximum price m3' },
+                    { func: min, label: 'Minimum price m3' },
+                    { func: variance, label: 'Variance price m3' },
+                  ].map(({ func, label }) => (
+                    <SimpleBlock
+                      key={label}
+                      mainText={func(
+                        electricity_data.map((x) => x.m3),
+                      ).toString()}
+                      subtleText={label}
+                    />
+                  ))}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="rounded-lg bg-vc-border-gradient p-px shadow-lg shadow-black/20">
+          <div className="rounded-lg bg-black p-3.5 lg:p-6">
+            <div className="space-y-9">
+              <div className="prose prose-sm prose-invert max-w-none">
+                <h1 className="text-xl font-bold">Graphs</h1>
+                <HistogramWithDistribution
+                  data={electricity_data.map((x) => x.m3)}
+                  binCount={5}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
